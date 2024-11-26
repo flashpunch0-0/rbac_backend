cf8ae1af3868ad566e2e6e17a21002b0a62c7d49
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-11-26 20:12:38 +0530

    created a model user.js  and added bcrpyt hashing

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const rbac_userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+//  using bcrypt to hash the pasword
+rbac_userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model("User", rbac_userSchema);

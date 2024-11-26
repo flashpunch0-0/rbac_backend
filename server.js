714f5b7241aa17ad1808cc91db34dd17202bc3c2
KRATIK_BOHRA <kratikbohra5@gmail.com>
2024-11-26 19:34:45 +0530

    installed dependencies and basic server code

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.listen(3010, () => {
+  console.log("Server is running on port:3010");
+});
